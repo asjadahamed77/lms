8a85b52f87aef75631fab620cb9df70b168565be
asjadahamed77 <ahamedasjad77@gmail.com>
2025-08-26 09:13:32 +0530

    Completed Student Subject UI

--- frontend/src/assets/student.js
+++ frontend/src/assets/student.js
@@ -1,89 +1,256 @@
-
+import file1 from "../assets/files/Form A.pdf";
+import file2 from "../assets/files/Form B.pdf";
 
 export const student = {
-    id: 1,
-    userId: "efrruihfirfnoifhirfehn",
-    name: "Mohamed Jiffry Asjad Ahamed",
-    nameWithInitials: "MJA Ahamed",
-    registrationNumber: "20APSE4839",
-    email: "mjaaahamed@com.ac.lk",
-    password: "asjad1234",
-    role: "student",
-    batchName: "2020/2021",
-    facultyName: "Faculty of Computing",
-    departmentName: "Department of Software Engineering",
-    subjects: [
+  id: 1,
+  userId: "efrruihfirfnoifhirfehn",
+  name: "Mohamed Jiffry Asjad Ahamed",
+  nameWithInitials: "MJA Ahamed",
+  registrationNumber: "20APSE4839",
+  email: "mjaaahamed@com.ac.lk",
+  password: "asjad1234",
+  role: "student",
+  batchName: "2020/2021",
+  facultyName: "Faculty of Computing",
+  departmentName: "Department of Software Engineering",
+  subjects: [
+    {
+      subjectId: "dhniudbdedff",
+      subjectName: "Data Structures and Algorithms",
+      subjectCode: "SE3104",
+      subjectSemester: 3,
+      assignments: [
         {
-            subjectId: "dhniudbdedff",
-            subjectName: "Data Structures and Algorithms",
-            subjectCode: "SE3104",
-            subjectSemester: 3,
-            
+          assignmentId: "nefnfnefnnnff",
+          assignmentTitle: "Large Language Models",
+          assignmentDescription:
+            "Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment.",
+          assignmentFiles: [
+            {
+              fileName: "Ajji.pdf",
+              file: file1,
+            },
+          ],
+          deadline: "2025-08-28T10:30:00Z",
         },
         {
-            subjectId: "dhniudbdedff",
-            subjectName: "Data Structures and Algorithms",
-            subjectCode: "SE3104",
-            subjectSemester: 4,
+            assignmentId: "nefnbdjdfnefnnnff",
+            assignmentTitle: "Large Language Models",
+            assignmentDescription:
+              "Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment.",
+            assignmentFiles: [
+              {
+                fileName: "Ajji.pdf",
+                file: file1,
+              },
+            ],
+            deadline: "2025-08-28T10:30:00Z",
+          },
+          {
+            assignmentId: "eeeedndndd",
+            assignmentTitle: "Large Language Models",
+            assignmentDescription:
+              "Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment.",
+            assignmentFiles: [
+              {
+                fileName: "Ajji.pdf",
+                file: file1,
+              },
+            ],
+            deadline: "2025-08-22T10:30:00Z",
+          },
+      ],
+      quizzes: [
+        {
+          quizId: "nefnfnefnnnff",
+          quizTitle: "Large Language Models",
+          quizDescription:
+            "Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment.",
+          quizFiles: [
+            {
+              fileName: "Ajji.pdf",
+              file: file1,
+            },
+          ],
+          deadline: "2025-08-28T10:30:00Z",
         },
         {
-            subjectId: "dhniudbdedff",
-            subjectName: "Data Structures and Algorithms",
-            subjectCode: "SE3104",
-            subjectSemester: 5,
+            quizId: "nefnfnefnnnff",
+            quizTitle: "Large Language Models",
+            quizDescription:
+              "Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment.",
+            quizFiles: [
+              {
+                fileName: "Ajji.pdf",
+                file: file1,
+              },
+            ],
+            deadline: "2025-08-28T10:30:00Z",
+          },
+          {
+            quizId: "nefnfnefnnnff",
+            quizTitle: "Large Language Models",
+            quizDescription:
+              "Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment.",
+            quizFiles: [
+              {
+                fileName: "Ajji.pdf",
+                file: file1,
+              },
+            ],
+            deadline: "2025-08-28T10:30:00Z",
+          },
+      ],
+      resources: [
+       { resourceTitle: "Main Concepts of Design Principles",
+        resourceFiles: [
+            {
+              fileName: "Software Design Concepts.pdf",
+              file: file1,
+            },
+            {
+              fileName: "Software Design Concepts.docs",
+              file: file2,
+            },
+          ],
         },
+        { resourceTitle: "Main Concepts of Design Principles",
+          resourceFiles: [
+              {
+                fileName: "Software Design Concepts.pdf",
+                file: file1,
+              },
+              {
+                fileName: "Software Design Concepts.docs",
+                file: file2,
+              },
+            ],
+          },
+        
+      
+      ],
+    },
+    {
+      subjectId: "dhniudbdsssedff",
+      subjectName: "Data Structures and Algorithms",
+      subjectCode: "SE3104",
+      subjectSemester: 4,
+      assignments: [
         {
-            subjectId: "dhniudbdedff",
-            subjectName: "Data Structures and Algorithms",
-            subjectCode: "SE3104",
-            subjectSemester: 3,
+          assignmentId: "nefnfnssefnnnff",
+          assignmentTitle: "Large Language Models",
+          assignmentDescription:
+            "Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment.",
+          assignmentFiles: [
+            {
+              fileName: "Ajji.pdf",
+              file: file2,
+            },
+          ],
+          deadline: "2025-08-22T10:30:00Z",
         },
+      ],
+      quizzes: [
+        {
+          quizId: "nefnfnssefnnnff",
+          quizTitle: "Large Language Models",
+          quizDescription:
+            "Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment.",
+          quizFiles: [
+            {
+              fileName: "Ajji.pdf",
+              file: file2,
+            },
+          ],
+          deadline: "2025-08-22T10:30:00Z",
+        },
+      ],
+      resources: {
+        resourceFiles: [
+          {
+            fileName: "Ajji.pdf",
+            file: file2,
+          },
+          {
+            fileName: "Ahd.pdf",
+            file: file1,
+          },
+        ],
+      },
+    },
+    {
+      subjectId: "dhniuqqeddbdedff",
+      subjectName: "Data Structures and Algorithms",
+      subjectCode: "SE3104",
+      subjectSemester: 5,
+      assignments: [
+        {
+          assignmentId: "nefnfnefdjjbdnnnff",
+          assignmentTitle: "Large Language Models",
+          assignmentDescription:
+            "Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment. Hello this is a description of the assignment.",
+          assignmentFiles: [
+            {
+              fileName: "Ajji.pdf",
+              file: file1,
+            },
+          ],
+          deadline: "2025-08-22T10:30:00Z",
+        },
+      ],
+    },
+    {
+      subjectId: "dhniudbdccsedff",
+      subjectName: "Data Structures and Algorithms",
+      subjectCode: "SE3104",
+      subjectSemester: 3,
+    },
 
-        {
-            subjectId: "dhniudbdedff",
-            subjectName: "Data Structures and Algorithms",
-            subjectCode: "SE3104",
-            subjectSemester: 1,
-        },
-        {
-            subjectId: "dhniudbdedff",
-            subjectName: "Data Structures and Algorithms",
-            subjectCode: "SE3104",
-            subjectSemester: 2,
-        },
-        {
-            subjectId: "dhniudbdedff",
-            subjectName: "Data Structures and Algorithms",
-            subjectCode: "SE3104",
-            subjectSemester: 3,
-        },{
-            subjectId: "dhniudbdedff",
-            subjectName: "Data Structures and Algorithms",
-            subjectCode: "SE3104",
-            subjectSemester: 2,
-        },
+    {
+      subjectId: "dhniudllsbdedff",
+      subjectName: "Data Structures and Algorithms",
+      subjectCode: "SE3104",
+      subjectSemester: 1,
+    },
+    {
+      subjectId: "dhniusjsfdbdedff",
+      subjectName: "Data Structures and Algorithms",
+      subjectCode: "SE3104",
+      subjectSemester: 2,
+    },
+    {
+      subjectId: "dhniuddnd3jnddn3bdedff",
+      subjectName: "Data Structures and Algorithms",
+      subjectCode: "SE3104",
+      subjectSemester: 3,
+    },
+    {
+      subjectId: "dhniuwededdbdedff",
+      subjectName: "Data Structures and Algorithms",
+      subjectCode: "SE3104",
+      subjectSemester: 2,
+    },
 
-        {
-            subjectId: "dhniudbdedff",
-            subjectName: "Data Structures and Algorithms",
-            subjectCode: "SE3104",
-            subjectSemester: 1,
-        },
-        {
-            subjectId: "dhniudbdedff",
-            subjectName: "Data Structures and Algorithms",
-            subjectCode: "SE3104",
-            subjectSemester: 3,
-        },{
-            subjectId: "dhniudbdedff",
-            subjectName: "Data Structures and Algorithms",
-            subjectCode: "SE3104",
-            subjectSemester: 8,
-        },
-
-    ],
-    createdAt: "2025-08-22T10:30:00Z",
-    lastLogin: "2025-09-22T12:30:00Z",
-    isActive: true,
-    
-}
+    {
+      subjectId: "dhniudbaddedxededcedfdedff",
+      subjectName: "Data Structures and Algorithms",
+      subjectCode: "SE3104",
+      subjectSemester: 1,
+    },
+    {
+      subjectId: "dhniwdudedwddbdedff",
+      subjectName: "Data Structures and Algorithms",
+      subjectCode: "SE3104",
+      subjectSemester: 3,
+    },
+    {
+      subjectId: "dhdwdniudbdedwdff",
+      subjectName: "Data Structures and Algorithms",
+      subjectCode: "SE3104",
+      subjectSemester: 8,
+    },
+  ],
+  createdAt: "2025-08-22T10:30:00Z",
+  lastLogin: "2025-09-22T12:30:00Z",
+  isActive: true,
+};
